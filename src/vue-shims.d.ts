6578c4ed21a0c29857546f372326a8cdd47f785b
jigsawye <jigsaw.ye@gmail.com>
2017-12-29 13:29:22 +0800

    Remove unused types

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -2,17 +2,3 @@ declare module "*.vue" {
     import Vue from "vue";
     export default Vue;
 }
-
-declare module 'element-ui/lib/locale' {
-    export function use(locale: string): void;
-}
-
-declare module 'element-ui/lib/locale/lang/zh-TW' {
-    const locale: string;
-    export default locale;
-}
-
-declare module "*.json" {
-    const value: any;
-    export default value;
-}
